a9a6ecf4088b63fcaee6b0acfadfd8ca1858410f
Jonathan <56617776+qb1968@users.noreply.github.com>
2019-11-12 18:53:24 -0500

    Round One

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, callback) {
+ return callback(list.length)
 }
 
 /**
@@ -66,8 +66,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringlist, callback) {
+  return callback(stringlist[0+1])
 }
 
 /**
@@ -87,10 +87,11 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+ return callback (numberList.reduce(function(accumulator, numberList) {
+   return accumulator + numberList;
+ }, 0))
 }
-
 /**
  * ### Challenge `processProduct`
  * 
@@ -109,8 +110,11 @@ function processSum(/* CODE HERE */) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+function processProduct(num1, num2, callback) {
+  return callback(num1,num2) 
+}
+const multiply = (num1, num2) =>{
+  return num1 * num2; 
 }
 
 /**
